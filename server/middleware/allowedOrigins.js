67f36b420606d1ac2f090539361f8283807a4d7b
Delyar Alahmad <149537823+Dell237@users.noreply.github.com>
2024-02-28 14:31:35 +0100

    modified:   server/middleware/allowedOrigins.js

--- server/middleware/allowedOrigins.js
+++ server/middleware/allowedOrigins.js
@@ -1,6 +1,5 @@
 const allowedOrigins = [
-  "https://www.yoursite.com",
-  "http://127.0.0.1:5173",
+  "https://mern-wb.vercel.app",
   "http://localhost:5000",
   "http://localhost:3000",
   "http://localhost:5173",
